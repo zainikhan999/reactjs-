7ad598b9e14de6f804edb478b864620e3e4c416e
zainikhan999 <zk.032918@gmail.com>
2024-08-27 00:00:45 +0500

    running apps

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>
+   <Chai/>
+   <h1>This is how fragment works</h1>
+   <p>this is test para</p>
+   </>
+
+  )
+}
+
+export default App
---
+++ reactproject/src/chai.jsx
@@ -0,0 +1,10 @@
+
+
+function Chai()
+{
+    return(
+        <h1>chai is ready</h1>
+    )
+}
+
+export default Chai
